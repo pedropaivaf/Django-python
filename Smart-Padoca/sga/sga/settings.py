a356681db9757d555ae10b270f2d213dd02611aa
Pedro Ferreira <pedro.p.ferreira@hotmail.com>
2024-11-19 14:39:37 -0300

    banco de dados integrado com sucesso

--- Smart-Padoca/sga/sga/settings.py
+++ Smart-Padoca/sga/sga/settings.py
@@ -71,20 +71,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sga.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PadariaInteligente',
+        'USER': 'postgres',
+        'PASSWORD': 'Paiva2004',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
